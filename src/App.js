9ced84d13ac77c2d8887f232090b240692a3f6f1
bikkimahato <bikkimahato123@gmail.com>
2019-09-20 10:42:51 +0530

    Router is added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandingScreen from './individualComponents/LandingScreen';
 
 
 
 class App extends React.Component {
-  render(){
-  return (
-    <div className="App">
-     <LandingScreen />
-   
-    </div>
-  );
-}}
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route exact path="/" component={LandingScreen} />
+          <Route path='/user_login' component={LoginScreen} />
+          <Route path='/admin_login' component={LoginScreen} />
+          <Route path='/userhome' component={UserHome} />
+          <Route path='/adminhome' component={AdminHome} />
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
