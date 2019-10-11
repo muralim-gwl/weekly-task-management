8ec337a42bf156e8bcb4b6b17e2b34716c752314
Sushant Kumar <36587557+sushkr34@users.noreply.github.com>
2019-10-11 18:07:29 +0530

    api is added
    api is added

--- src/individualComponents/UserHome/UserHome.js
+++ src/individualComponents/UserHome/UserHome.js
@@ -4,63 +4,74 @@ import MonthWeek from '../../commonComponents/MonthWeek/MonthWeek'
 import AddTaskScreen from '../AddTaskScreen/AddTaskScreen'
 import "./UserHome.css"
 import { withRouter, Link, Route } from 'react-router-dom'
+import axios from 'axios'
 
-const UserHome = ({ user, taskTransaction, months, weeks, dummyMonthValue, 
-    dummyWeekValue, weekRestrictionHandler, getWeek, dummyCredential, taskObj, 
-    handleChangeTask, handleChangeButton, deleteHandler, userCheckboxHandler, props }) => {
-    return (
+class UserHome extends React.Component {
+
+
+
+    
+    state = {
+        tasklist :[]
+    }
+    
+      GetTasks =() =>
+        {
+            const { setTasks } = this;
+            axios.post("http://localhost:8080/api/user_task_list",{
+           "user_id": "3",
+            "month": "October",
+            "week": "Week 2"
+        })
+        .then(function (response){
+          console.log(response.data.response,"ucvsdjcvdkjkjvv");
+          setTasks (response.data.response);
+
+         console.log( this.state.tasklist,"jhfvudsvbdsvbidsvgyouibvlcsbvyufvbwuivbcslkc")
+        })
+        .catch(function(error){
+          console.log(error);
+        });
+
+        console.log(setTasks,"111222233333")
+       
+
+       
+        }
+
+        setTasks = tasklist => {
+            this.setState({ tasklist });
+        }
+
+    componentDidMount(){
+        
+       this.GetTasks()
+   
+    
+    }
+   
+    render(){
+        const  { user, taskTransaction, months, weeks, dummyMonthValue, dummyWeekValue, weekRestrictionHandler, getWeek, dummyCredential, taskObj, handleChangeTask, handleChangeButton, deleteHandler, userCheckboxHandler}=this.props;
+        const{tasklist} = this.state;        
+       
+         return (
+            
+            
         <div className="user-home">
             <div >
                 <LogoHeader />
-                <MonthWeek months={months} weeks={weeks} dummyMonthValue={dummyMonthValue} 
-                    dummyWeekValue={dummyWeekValue} weekRestrictionHandler={weekRestrictionHandler}
-                    getWeek={getWeek} />
+                <MonthWeek months={months} weeks={weeks} dummyMonthValue={dummyMonthValue} dummyWeekValue={dummyWeekValue} weekRestrictionHandler={weekRestrictionHandler} getWeek={getWeek} />
                 <Link to='/userhome/addtask' ><button>Add</button></Link>
-                <Route path='/userhome/addtask' component={(props) => <AddTaskScreen 
-                    taskTransaction={taskTransaction} dummyCredential={dummyCredential} 
-                    dummyMonthValue={dummyMonthValue} dummyWeekValue={dummyWeekValue} taskObj={taskObj} 
-                    user={user} handleChangeTask={handleChangeTask} handleChangeButton={handleChangeButton}
-                    {...props} />} />
-                <div>
-                    {
-                        user.map((element, index) => {
-                            return (
-                                <div>
-                                    {taskTransaction.map((tvalue, tindex) => {
-                                        return (
-                                            <div>
-                                                {
-                                                    (tvalue.monthName === dummyMonthValue && 
-                                                     tvalue.weekName === dummyWeekValue &&
-                                                     element.username === dummyCredential.username &&
-                                                     tvalue.taskActive) ?
-                                                        <p style={{ background: 'blue', color: 'white' }}>{element.id === tvalue.userid ?
-                                                            <p>{tvalue.taskName + ': '}
-                                                                points:{tvalue.taskPoint} 
-                                                                <input type="checkbox" 
-                                                                 disabled={tvalue.taskStatus} 
-                                                                 onChange={() => userCheckboxHandler(tindex)}>
-                                                                 </input>
-                                                                <button onClick={() => deleteHandler(tindex)}>
-                                                                Delete
-                                                                </button>
-                                                            </p> :
-                                                             null}
-                                                        </p>
-                                                        : null
-                                                }
-                                            </div>
-                                        )
-                                    }
+                <Route path='/userhome/addtask' component={(props) => <AddTaskScreen taskTransaction={taskTransaction} dummyCredential={dummyCredential} dummyMonthValue={dummyMonthValue} dummyWeekValue={dummyWeekValue} taskObj={taskObj} user={user} handleChangeTask={handleChangeTask} handleChangeButton={handleChangeButton} {...props} />} />
 
-                                    )}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                {tasklist.map((data,index)=>(
+                    <p>{data.task_name}</p>
+
+                ))}
+
             </div>
         </div>
-    )
+    );
+    }
 }
 export default withRouter(UserHome);
