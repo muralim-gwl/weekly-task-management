5101b7b9c7e9884bd5fde03d4444c63aa6ceefd6
Nireesh <nireesh.v@goodworklabs.com>
2019-10-14 17:12:42 +0530

    Update UserHome.js
    api added for delete

--- src/individualComponents/UserHome/UserHome.js
+++ src/individualComponents/UserHome/UserHome.js
@@ -5,6 +5,7 @@ import AddTaskScreen from '../AddTaskScreen/AddTaskScreen'
 import "./UserHome.css"
 import { withRouter, Link, Route } from 'react-router-dom'
 import axios from 'axios'
+import { string } from 'prop-types'
 
 class UserHome extends React.Component {
 
@@ -14,7 +15,45 @@ class UserHome extends React.Component {
     state = {
         tasklist :[]
     }
+
+    //API CALLS
     
+    DeleteTask =(taskId,index, type) =>
+       {
+           const { tasklist = [] } = this.state
+           console.log(index,"inputindex")
+           let baseurl;
+           if(type == "button"){
+             baseurl = "http://localhost:8080/api/delete/";
+           }else{
+            baseurl = "http://localhost:8080/api/updatetaskstatus/";
+           }
+           baseurl = baseurl+taskId;
+           console.log(index)
+           debugger;
+           axios.put(baseurl)
+       .then(function (response){
+
+         console.log(response.data.status,"ucvsdjcvdkjkjvv");
+        if(response.data.status === "successfull"){
+            tasklist[index].status = false;
+            console.log(tasklist,"AfterUpdate")
+            window.location.reload(false);
+
+        }
+
+        this.setState({
+            ...tasklist,
+            tasklist
+        })
+
+
+       })
+       .catch(function(error){
+         console.log(error);
+       });
+
+       }
       GetTasks =() =>
         {
             const { setTasks } = this;
@@ -64,10 +103,20 @@ class UserHome extends React.Component {
                 <Link to='/userhome/addtask' ><button>Add</button></Link>
                 <Route path='/userhome/addtask' component={(props) => <AddTaskScreen taskTransaction={taskTransaction} dummyCredential={dummyCredential} dummyMonthValue={dummyMonthValue} dummyWeekValue={dummyWeekValue} taskObj={taskObj} user={user} handleChangeTask={handleChangeTask} handleChangeButton={handleChangeButton} {...props} />} />
 
-                {tasklist.map((data,index)=>(
-                    <p>{data.task_name}</p>
 
-                ))}
+
+
+                     {tasklist.map((data, index) => (
+                         
+                            ( data.status)?
+                            (<p>{data.task_name}
+                                 <input type="checkbox" onChange={() => this.DeleteTask(data.task_id, index,"checkbox")}></input>
+                                 <button value={data.task_id} onClick={() => this.DeleteTask(data.task_id, index,"button")}>delete</button>
+                             </p>) 
+                             : null
+
+                         
+                     ))}
 
             </div>
         </div>
