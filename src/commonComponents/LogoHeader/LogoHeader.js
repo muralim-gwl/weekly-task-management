977cb7722a4f3c179e8cfaf82f95ab8f614fdd79
Sushant Kumar <36587557+sushkr34@users.noreply.github.com>
2019-10-24 11:22:13 +0530

    logo header added to logoheader.js

--- src/commonComponents/LogoHeader/LogoHeader.js
+++ src/commonComponents/LogoHeader/LogoHeader.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './LogoHeader.css'
 import { Link } from 'react-router-dom'
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
 const LogoHeader = () => {
 
     return (
-        <div className='header-class'>
+        <div>
+         <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" style={{ flexGrow: "1", float: "left", textAlign: "left" }}  >
+              Weekly Task Management
+              </Typography>
+              <Link to='/'>
+              <Button variant="contained" color="secondary" style={{textDecoration:'none'}}  >
+                  Log Out
+              </Button></Link>
+          </Toolbar>
+        </AppBar>
+        {/* <div className='header-class'>
             <div><img className="left-img" src="foot.png" alt='Logo of GWL' /></div>
             <div><Link to='/'><button type='button' className='button'>Log Out</button></Link></div>
 
+        </div> */}
         </div>)
 }
 export default LogoHeader;
--- src/commonComponents/MonthWeek/MonthWeek.js
+++ src/commonComponents/MonthWeek/MonthWeek.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './MonthWeek.css'
+import { Select } from '@material-ui/core'
+
 
 const MonthWeek =({months, weeks, selectedMonth, selectedWeek,weekRestrictionHandler,getWeek})=> {
     
-  console.log(months, "total months")
-  console.log(weeks,"total weeks")
+//   console.log(months, "total months")
+//   console.log(weeks,"total weeks")
         return (
             <div className='month-display'>
                 <form>
-                    <label>Month:</label>
-                    <select value={selectedMonth} onChange={weekRestrictionHandler}>
+                    <label><b>Month:&nbsp; </b></label>
+                    <Select   value={selectedMonth} onChange={weekRestrictionHandler}>
                         {months.map(el => <option value={el} >{el}</option>)}
-                    </select>
-                    <label>Weeks:</label>
-                    <select value={selectedWeek} onChange={getWeek}>
+                    </Select> &nbsp;&nbsp;
+                    <label><b>Weeks:&nbsp;&nbsp;</b></label>
+                    <Select value={selectedWeek} onChange={getWeek}>
                         {weeks.map(el => <option value={el} >{el}</option>)}
-                    </select>
+                    </Select>
                 </form>
             </div>)
     }
